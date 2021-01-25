666238c928e70b6d783fc8a76672ac8785a4fa81
Jishan Ekram <ekram.jishan@gmail.com>
2021-01-25 13:56:57 -0500

    commit tree cba79bedf47f7f7a44c281fd877915ac73ea6d35

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/%22))
+doc.css(".site-header__hero__headline").text
